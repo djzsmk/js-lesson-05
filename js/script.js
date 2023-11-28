3e62514951ff6a710a9a21396fec09325367932e
Michael <lightbar200@gmail.com>
2023-11-27 19:17:11 -0600

    changed if and else

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ var button = document.querySelector (".show-cat");
 var cat = document.querySelector (".cat");
 
 button.addEventListener ("click" , function(){
-    //button.innerText = "Its working";
-    cat.classList.add ("show")
+    if (cat.classList.contains("show")) {
+        cat.classList.remove("show");
+    } else {
+        cat.classList.add("show")
+    }
 });
