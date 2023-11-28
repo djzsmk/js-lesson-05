ef5bc6dac1813384a4a11a42984f37be0ea6eff8
Michael <lightbar200@gmail.com>
2023-11-27 19:33:30 -0600

    added more to if else

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@ var cat = document.querySelector (".cat");
 button.addEventListener ("click" , function(){
     if (cat.classList.contains("show")) {
         cat.classList.remove("show");
+        button.innerText = ("Wait, come back!.")
+        button.classList.add("disappear")
     } else {
         cat.classList.add("show")
+        button.innerText = ("Shoo, cat!!")
+        button.classList.remove("disappear")
     }
 });
